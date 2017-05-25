67a4653c6622bc9c2f6544ea44359c38a42e158a
Erik Uhlmann <erikuhlmann@gmail.com>
2017-05-25 08:50:53 -0400

    clean up

--- Robot-Control/examples/BasicExample/BasicExample.ino
+++ Robot-Control/examples/BasicExample/BasicExample.ino
@@ -1,50 +1,23 @@
 #include <RobotControl.h>
 
 void setup() {
-  // automatically sets up Serial for you
-  // because Serial is required for wireless module communication
-  // it does Serial.begin(57600)
-  // 115200 doesn't work so 57600 is the next fastest baud rate
-  // we need 20 kbits/s for communication (21 floats * 4 bytes per float * 30 fps * 8 bits per byte)
-  // so regular 9600 baud doesn't work too well. If using serial monitor, set baud to 57600
   RobotControl.begin();
+  
+  pinMode(BUILTIN_LED, OUTPUT);
 }
 
-void loop() {
-  // required call at the top of the loop function:
-  RobotControl.loop();
-  // NB: avoid delay() because it will delay controller input!
-  
-  float leftXAxis = RobotControl.getAxis(LEFT_X);
-  // returns a value from -1.0 to 1.0
-  // all axes:
-  // LEFT_X
-  // LEFT_Y
-  // RIGHT_X
-  // RIGHT_Y
-  
-  float buttonA = RobotControl.getButton(A);
-  // returns 1.0 for pressed or 0.0 for not pressed
-  float leftTrigger = RobotControl.getButton(TRIGGER_LEFT);
-  // analog value from 0.0 to 1.0
-  // all buttons:
-  // A
-  // B
-  // X
-  // Y
-  // BUMPER_LEFT
-  // BUMPER_RIGHT
-  // TRIGGER_LEFT
-  // TRIGGER_RIGHT
-  // BACK
-  // START
-  // STICK_LEFT
-  // STICK_RIGHT
-  // HAT_NORTH
-  // HAT_SOUTH
-  // HAT_WEST
-  // HAT_EAST
-  // LOGITECH
-  
-  // NB: minimize floating-point math operations for maximum speed!
+void loopAuto() {
+  digitalWrite(BUILTIN_LED, HIGH);
+  delay(1000);
+  digitalWrite(BUILTIN_LED, LOW);
+  delay(1000);
+}
+
+void loopTeleop() {
+  float buttonPressed = RobotControl.getButton(A);
+  if (buttonPressed) {
+    digitalWrite(BUILTIN_LED, HIGH);
+  } else {
+    digitalWrite(BUILTIN_LED, LOW);
+  }
 }
